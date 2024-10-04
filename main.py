2f8d264cfa84bc771a899922459e1051c90230c0
flyingdita <2760156471@qq.com>
2024-10-04 11:53:12 +0100

    add function : select class_id

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import filedialog
 import cv2
 import os
+import threading
+import sys
 
 # 全局变量
 drawing = False  # 鼠标按下标志
@@ -9,10 +11,15 @@ ix, iy = -1, -1  # 起始坐标
 rectangles = []  # 存储矩形坐标
 origin_folder_paths = []
 output_folder_paths = []
+color = [0,255,0]
+class_id = 0
 
 # 获取鼠标标注结果
 def draw_rectangle(event, x, y, flags, param):
-    global ix, iy, drawing, rectangles
+    global ix, iy, drawing, rectangles, color
+
+
+    # print(color)
 
     if event == cv2.EVENT_LBUTTONDOWN:  # 鼠标左键按下
         drawing = True
@@ -21,12 +28,12 @@ def draw_rectangle(event, x, y, flags, param):
     elif event == cv2.EVENT_MOUSEMOVE:  # 鼠标移动
         if drawing:
             img_copy = img.copy()  # 复制图像
-            cv2.rectangle(img_copy, (ix, iy), (x, y), (0, 255, 0), 1)
+            cv2.rectangle(img_copy, (ix, iy), (x, y), color, 1)
             cv2.imshow('Image', img_copy)
 
     elif event == cv2.EVENT_LBUTTONUP:  # 鼠标左键抬起
         drawing = False
-        cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), 1)
+        cv2.rectangle(img, (ix, iy), (x, y), color, 1)
         rectangles.append((ix, iy, x, y))  # 保存矩形坐标
 
 # # 设置键盘事件
@@ -47,6 +54,11 @@ def select_folder(folder_path):
     print(folder_path)
     return folder_path
 
+def on_closing():
+    print("Closing application...")
+    root.destroy()
+    sys.exit()
+
 
 def function1():
     global origin_folder_paths
@@ -60,50 +72,94 @@ def function2():
     output_folder_paths = select_folder(output_folder_paths)
 
 
-def function3():
-    print("Function 3 is called")
-    global origin_folder_paths
-    print(origin_folder_paths)
-    if (origin_folder_paths!=[]) and (output_folder_paths!=[]):
-        global img, rectangles
-        file_paths = []
-        for root, dirs, files in os.walk(origin_folder_paths):
-            for file in files:
-                # 拼接文件的完整路径
-                full_path = os.path.join(root, file)
-                file_paths.append(full_path)
-        
+def run_function3():
+    thread = threading.Thread(target=function3, daemon=True)
+    thread.start()
 
-        # print(file_paths)
-        for index,img_path in enumerate(file_paths):
-            img_name = img_path.split("\\")[1]
-            # print(img_name)
-            save_img_path = output_folder_paths + '/' + img_name
-            txt_path = output_folder_paths + '/' + img_name.replace('.jpg','.txt')
-            img = cv2.imread(img_path)
-            # resize
-            img = cv2.resize(img,(960,540))
-            cv2.namedWindow('Image')
-            cv2.imshow('Image',img)
-            cv2.setMouseCallback('Image', draw_rectangle)
-            while True:
-                key = cv2.waitKey(1) & 0xFF
-                if key == ord('d'):  
-                    # print("yesyes")
-                    with open(txt_path, 'w') as f:
-                        for rect in rectangles:
-                            f.write(f"{rect}\n")
-                    print(f"Annotations saved to {txt_path}")
-                    cv2.imwrite(save_img_path,img)
-                    break
-            cv2.imshow('Image',img)
-            cv2.waitKey(1)
-            if index == len(file_paths)-1:
-                finnal_img = cv2.imread('finnal.png')
-                cv2.imshow('Image',finnal_img)
-                cv2.waitKey(0)
-    else:
-        print("origin and output folder select is essential !!! ")
+def function3():
+    try:
+        print("读取图片")
+        while True:
+            print("Thread working...")
+            global origin_folder_paths
+            print(origin_folder_paths)
+            if (origin_folder_paths!=[]) and (output_folder_paths!=[]):
+                global img, rectangles, color
+                file_paths = []
+                for root, dirs, files in os.walk(origin_folder_paths):
+                    for file in files:
+                        # 拼接文件的完整路径
+                        full_path = os.path.join(root, file)
+                        file_paths.append(full_path)
+                
+
+                # print(file_paths)
+                for index,img_path in enumerate(file_paths):
+                    img_name = img_path.split("\\")[1]
+                    # print(img_name)
+                    save_img_path = output_folder_paths + '/' + img_name
+                    txt_path = output_folder_paths + '/' + img_name.replace('.jpg','.txt')
+                    img = cv2.imread(img_path)
+                    # resize
+                    img = cv2.resize(img,(960,540))
+                    cv2.namedWindow('Image')
+                    cv2.imshow('Image',img)
+
+                    cv2.setMouseCallback('Image', draw_rectangle)
+                    while True:
+                        key = cv2.waitKey(1) & 0xFF
+                        if key == ord('d'):  
+                            # print("yesyes")
+                            with open(txt_path, 'w') as f:
+                                for rect in rectangles:
+                                    f.write(f"{rect}\n")
+                            print(f"Annotations saved to {txt_path}")
+                            cv2.imwrite(save_img_path,img)
+                            break
+                    cv2.imshow('Image',img)
+                    cv2.waitKey(1)
+                    if index == len(file_paths)-1:
+                        finnal_img = cv2.imread('finnal.png')
+                        cv2.imshow('Image',finnal_img)
+                        cv2.waitKey(0)
+            else:
+                print("origin and output folder select is essential !!! ")
+
+    except KeyboardInterrupt:
+        print("Thread interrupted!")
+    finally:
+        print("Exiting thread...")
+    
+# def run_function4():
+#     thread = threading.Thread(target=function4, daemon=True)
+#     thread.start()
+
+def function4():
+    global color 
+    print("Please slect color")
+    number = int(input("请输入number： "))
+    print(number)
+
+    if number == 1:
+        color = [255,0,0]
+    elif number == 2:
+        color = [0,0,255]
+
+    # while True:
+    #     key_color = cv2.waitKey(1) & 0xFF
+    #     if key_color == ord('1'):
+    #         # blue
+    #         color = [255,0,0]
+    #         class_id = 0
+    #         break
+    #     elif key_color == ord('2'):
+    #         # red
+    #         color = [0,0,255]
+    #         break
+    #     elif key_color == ord('\n'):
+    #         break
+
+    print("\n color: {color}",color)
 
 
 
@@ -125,9 +181,15 @@ if __name__  == "__main__":
     button2 = tk.Button(root, text="选择输出标注文件路径", command=function2)
     button2.pack(pady=10)
 
-    button3 = tk.Button(root, text="读取图片", command=function3)
+    button3 = tk.Button(root, text="读取图片", command=run_function3)
+    button3.pack(pady=10)
+
+    button3 = tk.Button(root, text="Slect class_id", command=function4)
     button3.pack(pady=10)
 
     # 进入主循环
     # root.bind('<Key>', on_key_press)
-    root.mainloop()
+    try:
+        root.mainloop()
+    except KeyboardInterrupt:
+        on_closing()
--- test.py
+++ test.py
@@ -1,14 +1,27 @@
+import tkinter as tk
+import threading
+import time
+import sys
 import cv2
-import os
+from tkinter import filedialog
+
 
 # 全局变量
 drawing = False  # 鼠标按下标志
 ix, iy = -1, -1  # 起始坐标
 rectangles = []  # 存储矩形坐标
+origin_folder_paths = []
+output_folder_paths = []
+color = [0,255,0]
+class_id = 0
 
-# 鼠标回调函数
+
+# 获取鼠标标注结果
 def draw_rectangle(event, x, y, flags, param):
-    global ix, iy, drawing, rectangles
+    global ix, iy, drawing, rectangles, color
+
+
+    # print(color)
 
     if event == cv2.EVENT_LBUTTONDOWN:  # 鼠标左键按下
         drawing = True
@@ -17,46 +30,92 @@ def draw_rectangle(event, x, y, flags, param):
     elif event == cv2.EVENT_MOUSEMOVE:  # 鼠标移动
         if drawing:
             img_copy = img.copy()  # 复制图像
-            cv2.rectangle(img_copy, (ix, iy), (x, y), (0, 255, 0), 1.5)
+            cv2.rectangle(img_copy, (ix, iy), (x, y), color, 1)
             cv2.imshow('Image', img_copy)
 
     elif event == cv2.EVENT_LBUTTONUP:  # 鼠标左键抬起
         drawing = False
-        cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), 1.5)
+        cv2.rectangle(img, (ix, iy), (x, y), color, 1)
         rectangles.append((ix, iy, x, y))  # 保存矩形坐标
 
-# 标注多张图片的函数
-def annotate_images(image_folder):
-    global img, rectangles
-    image_files = [f for f in os.listdir(image_folder) if f.endswith(('.jpg', '.png'))]
 
-    for image_file in image_files:
-        img = cv2.imread(os.path.join(image_folder, image_file))
-        rectangles = []  # 每张图片清空标注
-        cv2.namedWindow('Image')
-        
-        key = cv2.waitKey(1) & 0xFF
-        # if key == ord('q'):
+def select_folder(folder_path):
+    root = tk.Tk()
+    root.withdraw()  # 隐藏主窗口
+    folder_path = filedialog.askdirectory()  # 弹出文件夹选择对话框
+
+    print(folder_path)
+    return folder_path
+
+
+def function1():
+    global origin_folder_paths
+    print("Function 1 is called")
+    origin_folder_paths = select_folder(origin_folder_paths)
+    # print(origin_folder_paths)
+
+def function2():
+    global output_folder_paths
+    print("Function 2 is called")
+    output_folder_paths = select_folder(output_folder_paths)
+
+def function3():
+    try:
+        print("读取图片")
+        while
+        True:
             
-        cv2.setMouseCallback('Image', draw_rectangle)
 
-        while True:
-            cv2.imshow('Image', img)
-            key = cv2.waitKey(1) & 0xFF
-            # 按 'Esc' 键进入下一张
-            if key == 27:
-                break
-            # 按 's' 键保存标注
-            elif key == ord('w'):  
-                with open(f'{image_file}_annotations.txt', 'w') as f:
-                    for rect in rectangles:
-                        f.write(f"{rect}\n")
-                print(f"Annotations saved to {image_file}_annotations.txt")
-            elif key == ord('q'):
-                rectangles = rectangles[-1]
-        cv2.imshow('Image', img)
-        cv2.destroyAllWindows()
 
-# 调用标注函数，替换为你的图片文件夹路径
-origin_path = r'D:\repo\database\Test\origin/'
-annotate_images(origin_path)
+
+
+
+
+
+            print("Thread working...")
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Thread interrupted!")
+    finally:
+        print("Exiting thread...")
+
+def function4():
+    print("选择 class_id")
+
+def run_function3():
+    thread = threading.Thread(target=function3, daemon=True)
+    thread.start()
+
+def run_function4():
+    thread = threading.Thread(target=function4, daemon=True)
+    thread.start()
+
+def on_closing():
+    print("Closing application...")
+    root.destroy()
+    sys.exit()
+
+root = tk.Tk()
+root.protocol("WM_DELETE_WINDOW", on_closing)
+
+root.title("按钮功能示例")
+# 设置界面大小
+root.geometry('300x200')
+
+# 创建按钮，并将其与相应的函数绑定
+button1 = tk.Button(root, text="读取文件夹", command=function1)
+button1.pack(pady=10)
+
+button2 = tk.Button(root, text="选择输出标注文件路径", command=function2)
+button2.pack(pady=10)
+
+button3 = tk.Button(root, text="读取图片", command=run_function3)
+button3.pack(pady=10)
+
+button4 = tk.Button(root, text="Select class_id", command=run_function4)
+button4.pack(pady=10)
+
+try:
+    root.mainloop()
+except KeyboardInterrupt:
+    on_closing()
